5817a4f4d9eb599ec43789026e97288a87329c1e
unknown <juliana.martins@DES238.softexpert.local>
2014-04-09 11:37:59 -0300

    modified:   html 	modified:   log1.xml 	modified:   testejuli.js 	deleted:    1111.txt 	deleted:    build.xml 	deleted:    build/phpcs.xml 	deleted:    build/phpmd.xml 	deleted:    phpunit.xml.dist 	deleted:    src/Money.php 	deleted:    src/hohoTest.php 	deleted:    tests/MoneyTest.php

--- testejuli.js
+++ testejuli.js
@@ -1,4 +1,4 @@
-/*casper.start('http://aquilasvn:8080/view/PHP%20Projects/job/php-unit-baseclass/', function() { 
+casper.start('http://aquilasvn:8080/view/PHP%20Projects/job/php-unit-baseclass/', function() { 
       this.capture('html', undefined, {
         format: 'jpg',
         quality: 75
@@ -12,18 +12,4 @@
 casper.run(function() { 
   this.test.done(); 
   casper.test.renderResults(true, 0, 'log1.xml');
-}); */
-
-var phantomcss = require('phantomcss.js');
-phantomcss.init();
- 
-casper.start( 'http://aquilasvn:8080/view/PHP%20Projects/job/php-unit-baseclass/' ).
-    then(function(){
-
-        // do something
-        casper.click('button#open-dialog');
-
-        // Take a screenshot of the UI component
-        phantomcss.screenshot('#the-dialog', 'a screenshot of my dialog');
-
-    });
+}); 
